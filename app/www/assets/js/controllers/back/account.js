314aa0cf3af65d294e9b4127d78f1cfb0e5243b5
Seun Ogunsanya <oseun04@gmail.com>
2017-05-20 05:52:02 +0100

    trying to fix the edit email and password functionalities

--- app/www/assets/js/controllers/back/account.js
+++ app/www/assets/js/controllers/back/account.js
@@ -1,11 +1,39 @@
-﻿'use strict';
+'use strict';
 
 /* Controllers */
 
 angular.module('app')
-    .controller('AccountCtrl', ['$scope', 'userInfoFactory', function ($scope, userInfoFactory) {
+    .controller('AccountCtrl', ['$scope','$injector', 'userInfoFactory','API','ngAuthSettings', function ($scope, $injector, userInfoFactory,API,ngAuthSettings) {
+    	var serviceBase = ngAuthSettings.apiServiceBaseUri;
+        var $http;
         $scope.userInfo = userInfoFactory.userInfo;
         $scope.form = {};
+        
+        $scope.updateemail=function(){
+
+        	// form.email=userInfoFactory.userInfo.email;
+        	// form.password=userInfoFactory.userInfo.password;
+        	if($scope.email != "" && $scope.password != ""){
+
+        		$http = $http || $injector.get('$http');
+        		$http.post(serviceBase + ngAuthSettings.apiOAuthToken, data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+        		.success(function (response) {
+
+        		   console.log('editted');
+
+        		    deferred.resolve(response);
+        		})
+        		.error(function (err, status) {
+        		    console.log('something went wrong');
+        		    deferred.reject(err);
+        		});
+
+        	};
+        	
+        	return deferred.promise;
+        };
+        
+
 
     }])
 
